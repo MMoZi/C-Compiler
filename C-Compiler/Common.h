ea1ecbf2977c10ce8c5e78097e0bcb879926ddee
CHIQIU <2926207225@qq.com>
2020-10-11 19:47:01 +0800

    一个新的开始

---
+++ C-Compiler/Common.h
@@ -0,0 +1,19 @@
+#pragma once 
+
+#include<iostream>
+#include <algorithm>
+#include <map>//STL 映射容器
+#include <string>//字符串类
+#include <vector>//STL 动态数组容器
+#include <math.h>//定义数学函数
+#include <set>
+#include <stack>
+#include <queue>
+#include <unordered_set>
+#include <unordered_map>
+#include <fstream> 
+#include <sstream>   
+#include <stdio.h>
+#include <string.h>
+#include <iterator>
+using namespace std; 
---
+++ C-Compiler/main.cpp
@@ -0,0 +1,13 @@
+#include "Common.h"
+
+
+int main(int argc, char** argv)
+{
+    // 读取位置
+    string path;
+    // 保存位置
+    string savePath; 
+  
+
+    return 0;
+}
